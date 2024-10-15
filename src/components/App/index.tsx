61ce760da818191a0a3662f7f60b7982b4d7288e
alin <alin@snapwork.com>
2024-10-15 05:14:39 -0500

    fix: change the structure

---
+++ src/components/App/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ShowDropdown from "../../pages/showDropdown";
+function App() {
+  return (
+    <div style={{ margin: 0, maxWidth: 500, marginLeft: 200, marginTop: 200 }}>
+      <ShowDropdown />
+    </div>
+  );
+}
+export default App;
---
+++ src/pages/showDropdown/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Ball from '../../assets/ball.png'
+import Gamepad from '../../assets/gamepad.png'
+import Education from '../../assets/education.png'
+import Art from '../../assets/art.png'
+import Health from '../../assets/health.png'
+import Dropdown from '../../components/UI/dropdown'
+const ShowDropdown=()=> {
+  const items=[{
+    label: 'Education',
+    value: 'education',
+    icon: Education
+  },
+  {
+    label: 'Art',
+    value: 'art',
+    icon: Art
+  },
+  {
+    label: 'Sport',
+    value: 'sport',
+    icon: Ball
+  },
+  {
+    label: 'Games',
+    value: 'games',
+    icon: Gamepad
+  },
+  {
+    label: 'Health',
+    value: 'Health',
+    icon: Health
+  }]
+  const onChangeDropdown=slectedValues=>{
+    console.log('slectedValues##', slectedValues)
+  }
+  const onNewItemAdded=(newItem, allItems)=>{
+    console.log('newItem##',newItem,'All items##' ,allItems)
+  }
+  return (<Dropdown 
+          items={items} 
+          onChange={onChangeDropdown} 
+          onNewItemAdded={onNewItemAdded}   
+          defaultValue={[{
+            label: 'Games',
+            value: 'games',
+          },
+          {
+            label: 'Health',
+            value: 'Health',
+          }]}
+          /> );
+}
+
+export default ShowDropdown;
